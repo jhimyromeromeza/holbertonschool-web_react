3fc412607f99acfd902e0b94196821a099f1c458
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-11-10 08:42:47 -0500

    add struct

--- TypeScript/task-0/js/main.ts
+++ TypeScript/task-0/js/main.ts
@@ -6,33 +6,33 @@ interface student {
     location: string;
 }
 //creating two object of student
-let student1: student = {
+const student1: student = {
     firstName: 'juan',
     lastName: 'romero',
     age: 22,
     location: 'lima'
 };
 
-let student2: student = {
+const student2: student = {
     firstName: 'rony',
     lastName: 'gaspar',
     age: 40,
     location: 'pichanaki'
 }
 
-let studentsList: student[] = [student1, student2];
+const studentsList: student[] = [student1, student2];
 
-let tabla = document.createElement('table');
+const tabla = document.createElement('table');
 document.body.appendChild(tabla);
 
 studentsList.forEach((student) => {
-    let fila = document.createElement('tr');
+    const fila = document.createElement('tr');
 
-    let celdaNombre = document.createElement('td');
+    const celdaNombre = document.createElement('td');
     celdaNombre.textContent = student.firstName;
     fila.appendChild(celdaNombre);
 
-    let celdaLocation = document.createElement('td');
+    const celdaLocation = document.createElement('td');
     celdaLocation.textContent = student.location;
     fila.appendChild(celdaLocation);
 
