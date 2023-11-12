c2f94d194d4a99b3f0bdf4d7217699b74fb964c6
jhimyromeromeza <romeromezajhimy0@gmail.com>
2023-11-12 12:26:00 -0500

    correct append body

--- Webpack/task_0/src/index.js
+++ Webpack/task_0/src/index.js
@@ -2,6 +2,6 @@ import $ from "jquery";
 
 $(function(){
     $('body').append('<p>Holberton Dashboard</p>');
-    $('body').apeend('<p>Dashboard data for the students</p>');
+    $('body').append('<p>Dashboard data for the students</p>');
     $('body').append('<p>Copyright - Holberton School</p>');
 });
